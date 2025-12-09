3a5c755180186baff5908bb4175e480ef1f393bb
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-12-09 10:56:58 +0100

    minor: Added biome. Linted everything. Don't use a class when all methods are static

--- matrikkelenheter/index.js
+++ matrikkelenheter/index.js
@@ -1,57 +1,60 @@
-const { logger } = require('@vestfoldfylke/loglady')
-const TemplateClient = require('../lib/TemplateClient/TemplateClient')
-const Sjablong = require('@vtfk/sjablong') // Replace placeholders in templates
-const { matrikkelApi } = require('../config')
-const { MatrikkelClient } = require('../lib/KartverketMatrikkelAPI/MatrikkelClient')
+const { logger } = require("@vestfoldfylke/loglady")
+const Sjablong = require("@vtfk/sjablong") // Replace placeholders in templates
+const { matrikkelApi } = require("../config")
+const { MatrikkelClient } = require("../lib/KartverketMatrikkelAPI/MatrikkelClient")
+const { getTemplate } = require("../lib/TemplateClient/TemplateClient")
 
-module.exports = async function (context, req) {
-  logger.logConfig({
-    prefix: 'matrikkelenheter'
-  })
+module.exports = async (_context, req) => {
+	logger.logConfig({
+		prefix: "matrikkelenheter"
+	})
 
-  if (!req.body.matrikkelContext) {
-    req.body.matrikkelContext = {}
-  }
+	if (!req.body.matrikkelContext) {
+		req.body.matrikkelContext = {}
+	}
 
-  if (req.body.koordinatsystemKodeId !== undefined) {
-    req.body.matrikkelContext.koordinatsystemKodeId = req.body.koordinatsystemKodeId
-  }
+	if (req.body.koordinatsystemKodeId !== undefined) {
+		req.body.matrikkelContext.koordinatsystemKodeId = req.body.koordinatsystemKodeId
+	}
 
-  req.matrikkelContext = Sjablong.replacePlaceholders(TemplateClient.getTemplate('matrikkelContext.xml'), req.body.matrikkelContext)
+	req.matrikkelContext = Sjablong.replacePlaceholders(getTemplate("matrikkelContext.xml"), req.body.matrikkelContext)
 
-  // Oppretter en ny klient for å kontakte matrikkelen.
-  const client = new MatrikkelClient(matrikkelApi.MATRIKKELAPI_USERNAME, matrikkelApi.MATRIKKELAPI_PASSWORD, 'matrikkelapi/wsapi/v1/MatrikkelenhetServiceWS')
+	// Oppretter en ny klient for å kontakte matrikkelen.
+	const client = new MatrikkelClient(matrikkelApi.MATRIKKELAPI_USERNAME, matrikkelApi.MATRIKKELAPI_PASSWORD, "matrikkelapi/wsapi/v1/MatrikkelenhetServiceWS")
 
-  const result = await client.getMatrikkelPolygon(req, req.body.polygon)
+	const result = await client.getMatrikkelPolygon(req, req.body.polygon)
 
-  let units
-  if (result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return !== undefined) {
-    // If result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item is not an array make it an array (it should always be an array, but sometimes it's not :D)
-    if (!Array.isArray(result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item)) {
-      units = result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item?.value === undefined ? '' : [result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item?.value] // Denne kan være undefined, håndter det returns [ undefined ]
-    } else {
-      units = result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item.map(unit => unit.value)
-    }
-  } else {
-    units = result[0].findMatrikkelenheterResponse?.return?.item.map(unit => unit.value)
-  }
+	let units
+	if (result[0]["soap:Envelope"]?.["soap:Body"].findMatrikkelenheterResponse.return !== undefined) {
+		// If result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item is not an array make it an array (it should always be an array, but sometimes it's not :D)
+		if (!Array.isArray(result[0]["soap:Envelope"]?.["soap:Body"].findMatrikkelenheterResponse.return.item)) {
+			units =
+				result[0]["soap:Envelope"]?.["soap:Body"].findMatrikkelenheterResponse.return.item?.value === undefined
+					? ""
+					: [result[0]["soap:Envelope"]?.["soap:Body"].findMatrikkelenheterResponse.return.item?.value] // Denne kan være undefined, håndter det returns [ undefined ]
+		} else {
+			units = result[0]["soap:Envelope"]?.["soap:Body"].findMatrikkelenheterResponse.return.item.map((unit) => unit.value)
+		}
+	} else {
+		units = result[0].findMatrikkelenheterResponse?.return?.item.map((unit) => unit.value)
+	}
 
-  if (units === undefined || units.length < 0) throw new Error('Fant ingen enheter innenfor polygonet')
+	if (units === undefined || units.length < 0) throw new Error("Fant ingen enheter innenfor polygonet")
 
-  logger.info("Got {MatrikkelUnitCount} units from matrikkel MatrikkelenhetServiceWS", units.length)
+	logger.info("Got {MatrikkelUnitCount} units from matrikkel MatrikkelenhetServiceWS", units.length)
 
-  try {
-    return {
-      status: 200,
-      body: {
-        units,
-        koordinatsystemKodeId: req.body.koordinatsystemKodeId
-      }
-    }
-  } catch (error) {
-    return {
-      status: 500,
-      body: error
-    }
-  }
+	try {
+		return {
+			status: 200,
+			body: {
+				units,
+				koordinatsystemKodeId: req.body.koordinatsystemKodeId
+			}
+		}
+	} catch (error) {
+		return {
+			status: 500,
+			body: error
+		}
+	}
 }
--- storeService/index.js
+++ storeService/index.js
@@ -1,43 +1,43 @@
-const { logger } = require('@vestfoldfylke/loglady')
-const Sjablong = require('@vtfk/sjablong') // Replace placeholders in templates
-const TemplateClient = require('../lib/TemplateClient/TemplateClient')
-const { matrikkelApi } = require('../config')
-const { MatrikkelClient } = require('../lib/KartverketMatrikkelAPI/MatrikkelClient')
-const { flattenObject } = require('../lib/helpers/flattenObject')
+const { logger } = require("@vestfoldfylke/loglady")
+const Sjablong = require("@vtfk/sjablong") // Replace placeholders in templates
+const { matrikkelApi } = require("../config")
+const { flattenObject } = require("../lib/helpers/flattenObject")
+const { MatrikkelClient } = require("../lib/KartverketMatrikkelAPI/MatrikkelClient")
+const { getTemplate } = require("../lib/TemplateClient/TemplateClient")
 
-module.exports = async function (context, req) {
-  logger.logConfig({
-    prefix: 'storeService'
-  })
+module.exports = async (_context, req) => {
+	logger.logConfig({
+		prefix: "storeService"
+	})
 
-  if (!req.body.matrikkelContext) {
-    req.body.matrikkelContext = {}
-  }
+	if (!req.body.matrikkelContext) {
+		req.body.matrikkelContext = {}
+	}
 
-  if (req.body.koordinatsystemKodeId !== undefined) {
-    req.body.matrikkelContext.koordinatsystemKodeId = req.body.koordinatsystemKodeId
-  }
+	if (req.body.koordinatsystemKodeId !== undefined) {
+		req.body.matrikkelContext.koordinatsystemKodeId = req.body.koordinatsystemKodeId
+	}
 
-  req.matrikkelContext = Sjablong.replacePlaceholders(TemplateClient.getTemplate('matrikkelContext.xml'), req.body.matrikkelContext)
+	req.matrikkelContext = Sjablong.replacePlaceholders(getTemplate("matrikkelContext.xml"), req.body.matrikkelContext)
 
-  const client = new MatrikkelClient(matrikkelApi.MATRIKKELAPI_USERNAME, matrikkelApi.MATRIKKELAPI_PASSWORD, 'matrikkelapi/wsapi/v1/StoreServiceWS')
-  const store = await client.callStoreService(req, req.body.items)
+	const client = new MatrikkelClient(matrikkelApi.MATRIKKELAPI_USERNAME, matrikkelApi.MATRIKKELAPI_PASSWORD, "matrikkelapi/wsapi/v1/StoreServiceWS")
+	const store = await client.callStoreService(req, req.body.items)
 
-  const flattStore = flattenObject(store)
-  const returnTypeCountObject = client.getReturnTypeCountObject(flattStore)
-  logger.info("Got data from matrikkel StoreServiceWS: {@Data}", returnTypeCountObject)
+	const flattStore = flattenObject(store)
+	const returnTypeCountObject = client.getReturnTypeCountObject(flattStore)
+	logger.info("Got data from matrikkel StoreServiceWS: {@Data}", returnTypeCountObject)
 
-  try {
-    return {
-      status: 200,
-      body: {
-        store: flattStore
-      }
-    }
-  } catch (error) {
-    return {
-      status: 500,
-      body: error
-    }
-  }
+	try {
+		return {
+			status: 200,
+			body: {
+				store: flattStore
+			}
+		}
+	} catch (error) {
+		return {
+			status: 500,
+			body: error
+		}
+	}
 }
